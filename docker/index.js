a693391f76169024e1d1ab4e608c871d9569812c
ALI-creator307 <asadali719310@gmail.com>
2026-08-27 22:17:22 +0500

    setup

---
+++ docker/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const port = process.env.PORT || 5000
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ msg: "server is running" })
+})
+
+app.listen(port, () => {
+    console.log("server is running on port", port)
+})
