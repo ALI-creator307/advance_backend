06125aea0647b9967ad0a7aa288412ffbc684fbe
ALI-creator307 <asadali719310@gmail.com>
2026-08-29 13:40:03 +0500

    add environment variable in docker-compose.yml

--- docker/phase1/index.js
+++ docker/phase1/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.status(200).json({ msg: "server is running" })
+    return res.status(200).json({ msg: "route is workng" })
 })
 
 app.listen(port, () => {
