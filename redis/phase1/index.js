60e854510e25e0df2e7f3885f433a877322b3b8a
ALI-creator307 <asadali719310@gmail.com>
2026-09-04 15:04:34 +0500

    create api for get or create user

--- redis/phase1/index.js
+++ redis/phase1/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./lib/db.js";
+import User from "./model/user.model.js";
 dotenv.config();
 
 
@@ -8,8 +9,32 @@ const port = process.env.PORT || 5000
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     return res.status(200).json({ msg: "route is workng" })
+})
+
+//make create api
+app.post('/create', async (req, res) => {
+
+    const { name, email, pass } = req.body;
+
+    const user = await User.create({
+        name, email, pass
+    })
+
+    return res.json(user)
+
+})
+
+//make get api
+app.post('/create', async (req, res) => {
+
+    const user = await User.find({})
+
+    return res.json(user)
+
 })
 
 app.listen(port, () => {
